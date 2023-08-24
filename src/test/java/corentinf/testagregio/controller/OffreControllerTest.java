8616308871a5c6d39e9eaccd6ac784ed7e3731b9
Corentin Ferrier <ferrier.corentin@wanadoo.fr>
2023-08-24 14:14:34 +0200

    Add get all offer by market service

--- src/test/java/corentinf/testagregio/controller/OffreControllerTest.java
+++ src/test/java/corentinf/testagregio/controller/OffreControllerTest.java
@@ -7,6 +7,7 @@ import corentinf.testagregio.model.TypeParc;
 import corentinf.testagregio.model.domain.BlocHoraireDomain;
 import corentinf.testagregio.model.domain.OffreDomain;
 import corentinf.testagregio.model.dto.BlocHoraireDto;
+import corentinf.testagregio.model.dto.OffreDto;
 import corentinf.testagregio.model.dto.ParcDto;
 import corentinf.testagregio.service.OffreService;
 import corentinf.testagregio.service.ParcService;
@@ -20,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDateTime;
 import java.util.Collections;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -34,6 +36,7 @@ public class OffreControllerTest {
 
     @Autowired
     private OffreService offreService;
+
 
     @Test
     void saveOffre() throws Exception {
@@ -66,5 +69,29 @@ public class OffreControllerTest {
                         .content(offreDomainAsJson))
                 .andExpect(status().isOk())
                 .andReturn();
+    }
+
+    @Test
+    void getAllOffreByMarket() throws Exception {
+        var parcDto = ParcDto.builder()
+                .typeParc(TypeParc.EOLIEN)
+                .blocsHoraire(Collections.singletonList(BlocHoraireDto.builder()
+                        .quantiteEnergieTotale(100)
+                        .debut(LocalDateTime.of(2023, 8, 20, 12, 0))
+                        .build()))
+                .build();
+
+        parcService.saveParc(parcDto);
+
+        var offreDto1 = OffreDto.builder()
+                .typeMarche(TypeMarche.RESERVE_PRIMAIRE)
+                .prixPlancher(5000)
+                .build();
+
+        offreService.saveOffre(offreDto1);
+
+        this.mockMvc.perform(get("/offres/by-market").contentType(MediaType.APPLICATION_JSON)
+                        .requestAttr("typeMarche", TypeMarche.RESERVE_PRIMAIRE))
+                .andExpect(status().isOk());
     }
 }
--- src/test/java/corentinf/testagregio/service/OffreServiceTest.java
+++ src/test/java/corentinf/testagregio/service/OffreServiceTest.java
@@ -5,6 +5,9 @@ import corentinf.testagregio.model.TypeParc;
 import corentinf.testagregio.model.dto.BlocHoraireDto;
 import corentinf.testagregio.model.dto.OffreDto;
 import corentinf.testagregio.model.dto.ParcDto;
+import corentinf.testagregio.repository.OffreRepository;
+import corentinf.testagregio.repository.ParcRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +24,18 @@ class OffreServiceTest {
 
     @Autowired
     private OffreService offreService;
+
+    @Autowired
+    private ParcRepository parcRepository;
+
+    @Autowired
+    private OffreRepository offreRepository;
+
+    @BeforeEach
+    public void before() {
+        offreRepository.deleteAll();
+        parcRepository.deleteAll();
+    }
 
     @Test
     void saveOffre() {
@@ -49,5 +64,41 @@ class OffreServiceTest {
 
         assertEquals(savedOffreDto.getParc().getId(), parcDto.getId());
         assertEquals(savedOffreDto.getBlocsHoraire().get(0).getQuantiteEnergieAlouee(), 0);
+    }
+
+    @Test
+    void findAllByMarket() {
+        var parcDto = ParcDto.builder()
+                .typeParc(TypeParc.EOLIEN)
+                .blocsHoraire(Collections.singletonList(BlocHoraireDto.builder()
+                        .quantiteEnergieTotale(100)
+                        .debut(LocalDateTime.of(2023, 8, 20, 12, 0))
+                        .build()))
+                .build();
+
+        parcService.saveParc(parcDto);
+
+        var offreDto1 = OffreDto.builder()
+                .typeMarche(TypeMarche.RESERVE_PRIMAIRE)
+                .prixPlancher(5000)
+                .build();
+
+        var offreDto2 = OffreDto.builder()
+                .typeMarche(TypeMarche.RESERVE_RAPIDE)
+                .prixPlancher(6000)
+                .build();
+
+        var offreDto3 = OffreDto.builder()
+                .typeMarche(TypeMarche.RESERVE_SECONDAIRE)
+                .prixPlancher(7000)
+                .build();
+
+        offreService.saveOffre(offreDto1);
+        offreService.saveOffre(offreDto2);
+        offreService.saveOffre(offreDto3);
+
+        var offreByMarket = offreService.findAllByMarket(TypeMarche.RESERVE_PRIMAIRE).get(0);
+        assertEquals(offreByMarket.getTypeMarche(), TypeMarche.RESERVE_PRIMAIRE);
+        assertEquals(offreByMarket.getPrixPlancher(), 5000);
     }
 }
